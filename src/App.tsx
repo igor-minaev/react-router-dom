dec0a80e41871c066703e5a907653b8ddc2c1065
Igor Minayeu <igor.minayeu@gmail.com>
2023-02-02 18:18:23 +0300

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
-import {NavLink, Route, Routes} from "react-router-dom";
+import {Navigate, NavLink, Route, Routes, useNavigate} from "react-router-dom";
+
+const Profile = () => {
+    const navigate = useNavigate()
+    /*useEffect(() => {
+        if (true) navigate('/login')
+    }, [])*/
+
+    return (
+        <div>
+            {true ? (<Navigate to={'/login'}/>)
+                : (
+                    <>
+                        Profile
+                        <button onClick={() => {navigate('/login')}}>logout</button>
+                    </>
+                )
+            }
+
+        </div>
+    )
+}
 
 function App() {
     return (
         <div className="App">
             <NavLink to={'/'}>Main</NavLink>---
             <NavLink to={'/login'}>Login</NavLink>---
-
-            <NavLink
-                to={'/profile'}
-                style={({isActive}) => ({color: isActive ? 'lime' : 'black'})
-                }
-            >
-                Profile
-            </NavLink>---
-            <NavLink
-                to={'/profile/settings'}
-            >
-                Settings
-            </NavLink>
+            <NavLink to={'/profile'}>Profile</NavLink>---
+            <NavLink to={'/profile/settings'}>Settings</NavLink>
             <Routes>
                 <Route path={'/*'} element={<div>404</div>}/>
                 <Route path={'/'} element={<div>Main</div>}/>
                 <Route path={'/login'} element={<div>Login</div>}/>
-                <Route path={'/profile'} element={<div>Profile</div>}/>
+                <Route path={'/profile'} element={<Profile/>}/>
                 <Route path={'/profile/settings'} element={<div>Settings</div>}/>
 
             </Routes>
