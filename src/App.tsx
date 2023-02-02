408199bc205dd5641e1f99fab435e4b071cd1512
Igor Minayeu <igor.minayeu@gmail.com>
2023-02-02 18:01:15 +0300

    links

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {NavLink, Outlet, Route, Routes, useParams} from "react-router-dom";
-
-const Profile = () => {
-    const params = useParams<'*'>()
-    const some = params
-    console.log(some)
-    return <div>Profile</div>
-}
-
+import {NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
             <NavLink to={'/'}>Main</NavLink>---
             <NavLink to={'/login'}>Login</NavLink>---
-            <NavLink to={'/profile'}>Profile</NavLink>---
-            <NavLink to={'/profile/1'}>Profile/1</NavLink>
+
+            <NavLink
+                to={'/profile'}
+                style={({isActive}) => ({color: isActive ? 'lime' : 'black'})
+                }
+            >
+                Profile
+            </NavLink>---
+            <NavLink
+                to={'/profile/settings'}
+            >
+                Settings
+            </NavLink>
             <Routes>
                 <Route path={'/*'} element={<div>404</div>}/>
                 <Route path={'/'} element={<div>Main</div>}/>
-                <Route path={'/login/*'} element={<div>Login</div>}/>
-                <Route path={'/profile/*'} element={<Profile/>}/>
+                <Route path={'/login'} element={<div>Login</div>}/>
+                <Route path={'/profile'} element={<div>Profile</div>}/>
+                <Route path={'/profile/settings'} element={<div>Settings</div>}/>
 
-                {/*<Route path={'/*'} element={<div>404</div>}/>
-                <Route path={'/'} element={<div>Main</div>}/>
-                <Route path={'/login/*'} element={<div>Login</div>}/>
-                <Route path={'/profile/*'} element={(
-                    <div>
-                        Profile
-                        <Routes>
-                            <Route path={'/settings'} element={<div>Settings</div>}/>
-                        </Routes>
-                    </div>
-                )}/>*/}
             </Routes>
         </div>
     );
